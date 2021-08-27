4352796eff22d0d1939cba6f123609106bfaa35c
MaviaAli <maviaali597@gmail.com>
2021-08-28 02:04:29 +0500

    AI backup

---
+++ Maths.py
@@ -0,0 +1,13 @@
+#All these functions are imported from text file Maths.py to (AI) Lesson 3.
+
+def add(a,b):
+    return a + b
+
+def sub(a,b):
+    return a - b
+
+def mult(a,b):
+    return a * b
+
+def div(a,b):
+    return a / b
